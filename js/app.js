0e1c4e3462fbbf51fbe1f969ccce109bd41a0a4d
Roy-9L <l>
2019-09-20 10:46:00 +0800

    Site updated: 2019-09-20 10:45:56

--- js/app.js
+++ js/app.js
@@ -1,29 +1,50 @@
-var app = (function () {
-  var loadBanner = function () {
-    var bannerSmall = document.querySelector('.banner-small')
-
-    // can't just listen the bannerSmall's load event
-    // but why ?
-    var img = new Image()
-    img.src = bannerSmall.src
-    img.addEventListener('load', function (e) {
-      bannerSmall.classList.add('loaded')
-    }, false)
-
-    var bannerLarge = new Image()
-    bannerLarge.src = bannerSmall.dataset.src
-    bannerLarge.addEventListener('load', function (e) {
-      e.target.classList.add('loaded')
-    }, false)
-
-    bannerSmall.parentNode.appendChild(bannerLarge)
+const getRealPath = (pathname, desc = false) => {
+  if (!pathname) {
+    pathname = window.location.pathname;
   }
-
-  return {
-    loadBanner: loadBanner
+  let names = pathname.split("/");
+  if (desc === false) {
+    for (let i = names.length - 1; i >= 0; --i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
+      }
+    }
+  } else {
+    for (let i = 0; i < names.length; ++i) {
+      let name = names[i].trim();
+      if (name.length > 0 && name !== "/" && name !== "index.html") {
+        return name;
+      }
+    }
   }
-})()
+  return "/";
+};
+let links = document.querySelectorAll(".nexmoe-list-item");
+let rootRealPath = getRealPath(window.location.pathname, true);
+for (let link of links) {
+  let linkPath = link.getAttribute("href");
+  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+    link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
+  }
+}
+$("table")
+  .has("img")
+  .addClass("nexmoe-album");
 
-window.addEventListener('load', function (e) {
-  app.loadBanner()
-}, false)
+$("#nexmoe-content img").each(function() {
+  $(this).attr("data-src", $(this).attr("src"));
+  $(this).attr("src", "");
+  $(this).addClass("lazyload");
+  $(this).attr("referrerPolicy", "no-referrer");
+});
+
+$("article:not(.nexmoe-py) img").each(function() {
+  var element = document.createElement("a");
+  $(element).attr("data-fancybox", "gallery");
+  $(element).attr("href", $(this).attr("data-src"));
+  $(this).wrap(element);
+});
+
+$("#nexmoe-sidebar a").addClass("mdui-ripple");
+mdui.mutation();
